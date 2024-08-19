57d4075a9d49660b1ba003c9863c46cd688d2143
George Guimarães <george.guimaraes@gmail.com>
2024-08-19 19:04:27 -0300

    Move Nx.global_default_backend to test_helper.exs

--- lib/soothsayer/application.ex
+++ lib/soothsayer/application.ex
@@ -2,11 +2,7 @@ defmodule Soothsayer.Application do
   use Application
 
   def start(_type, _args) do
-    Nx.global_default_backend(EXLA.Backend)
-
-    children = [
-      # Add any necessary children here
-    ]
+    children = []
 
     opts = [strategy: :one_for_one, name: Soothsayer.Supervisor]
     Supervisor.start_link(children, opts)
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
+Nx.global_default_backend(EXLA.Backend)
 ExUnit.start(timeout: 600_000)
