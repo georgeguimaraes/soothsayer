7ddd5da9ace6a1129de8637113ff58315328799e
George Guimarães <george.guimaraes@gmail.com>
2024-08-21 09:55:35 -0300

    Add mix.exs goodies for publication

--- mix.exs
+++ mix.exs
@@ -1,17 +1,21 @@
 defmodule Soothsayer.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
+  @source_url "https://github.com/georgeguimaraes/soothsayer"
+
   def project do
     [
       app: :soothsayer,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,13 +23,34 @@ defmodule Soothsayer.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:explorer, "~> 0.9.1"},
       {:nx, "~> 0.7.3"},
       {:axon, "~> 0.6.1"},
       {:exla, "~> 0.7.3"}
+    ]
+  end
+
+  defp package do
+    [
+      description:
+        "Soothsayer is an Elixir library for time series forecasting, inspired by Facebook's Prophet and NeuralProphet.",
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
